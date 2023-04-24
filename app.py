c7f45b58c8f55f4e245e46325be62a11c7c13410
jcdeoliveirak <78385126+jcdeoliveirak@users.noreply.github.com>
2023-04-23 23:20:34 -0300

    versaofinal02

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Versão Final 23/04/2023"
+    return "Versão Final 23/04/2023 1"
 
 if __name__ == '__main__':
     app.run()
